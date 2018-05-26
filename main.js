9fbd28a4f0e6c0c0fbfd833202ee9a05804c297a
Radosław <radziowojak@gmail.com>
2018-05-26 15:56:59 +0200

    zrobione kopiowanie i wklejanie pytań

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 function wklej(index, question)
 {
-	if(index == 0)
-		$('#pytania').append("\""+question+"\"");
-	else
-		$('#pytania').append(",<br>\""+question+"\"");
+	if(index >1)
+		$('#pytania').append(",<br>");
+	$('#pytania').append("//question "+index+"<br>\""+question+"\"");
 }
 function skopiuj()
 {
@@ -12,7 +11,8 @@ function skopiuj()
 		fake=0,
 		isImg = false,
 		isPT = false,
-		isBlank = false;
+		isBlank = false,
+		isFlash = false;
 	//$("body").prepend("<div id='odpowiedzi' style='position:relative; background-color: white; border-bottom: 10px solid black'></div>")
 	$("body").prepend("<div id='pytania' style='position:relative; background-color: white; border-bottom: 10px solid black'></div>")
 	$('.post-content>p').each(function(i) 
@@ -31,28 +31,39 @@ function skopiuj()
 		if(isFlash)
 		{
 			fake--;
-			console.log(i-fake+1+': ///////////////////flash///////////////////')
-			//wklej(i-fake+1, '///////////////////flash///////////////////');
+			//console.log(i-fake+1+': ///////////////////flash///////////////////')
+			wklej(i-fake+1, '///////////////////flash///////////////////');
 		}
 		else if(isBlank)
 		{
 			fake--;
-			console.log(i-fake+1+': ///////////////////blank///////////////////')
-			//wklej(i-fake+1, '///////////////////blank///////////////////');
+			//console.log(i-fake+1+': ///////////////////blank///////////////////')
+			wklej(i-fake+1, '///////////////////blank///////////////////');
 		}
 		else
 		{
 			$(this).find('strong').each(function(j)
 			{
 				pytanie = $(this).text();
-				if((j == 1 && !isImg) || (j == 0 && isImg) )
+				if(!$(this).parent('span').length && !$(this).has('span').length && !pytanie.includes('not scored'))
 				{
-					console.log(pytanie);
+					if((j == 1 && !isImg && !isPT) || (j == 0 && isImg) || (j == 2 && isPT))
+					{
+						fake--;
+						//console.log(i-fake+1+": "+pytanie);
+						wklej(i-fake+1, pytanie)
+					}
+					else if(pytanie.indexOf('.') < 4 && pytanie.indexOf('.') >-1)
+					{
+						pytanie = pytanie.slice(pytanie.indexOf('.')+2); //szuka kropki i zostawia tekst od kropki +2 pola, czyli od początku pytania
+						fake--;
+						//console.log(i-fake+1+": "+pytanie);
+						wklej(i-fake+1, pytanie)
+					}
 				}
 			});
 		}
-		isImg = isPT = isBlank = false;
-		//pytanie = $(this).text().slice( $(this).text().indexOf('.')+2 ); //szuka kropki i zostawia tekst od kropki +2 pola, czyli od początku pytania
+		isImg = isPT = isBlank = isFlash = false;
 	});
 	/*$(".post-content>ul").each(function(i) 
 	{
